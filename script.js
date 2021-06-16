e781c0f5a037edba17f885944e223a51c3279d6c
walybi <walycode@gmail.com>
2021-06-16 08:23:24 -0700

    set up variables and create function to capture lenght value

--- script.js
+++ script.js
@@ -8,5 +8,23 @@ var symbols = document.getElementById("symbol");
 var generate = document.getElementById("generatebtn");
 var copy = document.getElementById("copybtn");
 
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var allNumbers = "0123456789";
+var specialSymbols = "!@#$%^&*()_+";
+
+var criteria = "";
+var password = "";
 
 
+
+
+// setting the user password lenght using mouse.
+slider.onclick = function() {
+    lenght.innerHTML = "Lenght: " + sliderValue.value;
+  };
+
+// setting the user password lenght using keyboad arroy keys.
+slider.onkeydown = function() {
+    lenght.innerHTML = "Lenght: " + sliderValue.value;
+  };
