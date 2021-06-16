6716d81d593cdcca8d7fc3b1f5ebda5dc2dd729b
walybi <walycode@gmail.com>
2021-06-16 00:47:24 -0700

    modifying files

---
+++ script.js
@@ -0,0 +1,12 @@
+var display = document.getElementById("displayWindow");
+var lenght = document.getElementById("lenghtValue");
+var slider = document.getElementById("sliderValue");
+var low = document.getElementById("lowecase");
+var up = document.getElementById("uppercase");
+var numbers = document.getElementById("number");
+var symbols = document.getElementById("symbol");
+var generate = document.getElementById("generatebtn");
+var copy = document.getElementById("copybtn");
+
+
+
